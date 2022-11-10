e0cbd321379de1cd2c5b009cb88fb3963084870a
thomasesteban22 <thomas.esteban@gmail.com>
2022-11-10 11:21:07 -0500

    gh pages actualization

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
+var ghpages = require('gh-pages');
 
 const fs = require("fs");
 const csv = require('csvtojson');
 const { Parser } = require('json2csv');
 
 app.use(express.static(__dirname + '/'));
+
+ghpages.publish('dist', function(err) {});
 
 app.get('/', (req, res) => {
    res.render('index');
@@ -18,7 +20,10 @@ app.set('view engine', 'html');
 module.exports = app;
 app.listen(3000, ()=>console.log("Puerto encendido::3000"));
 
-
+ghpages.publish('dist', {
+   branch: 'master',
+   repo: 'https://github.com/thomasesteban22/proyectoAriquitectura'
+},);
 
 if (typeof document !== "undefined") {
    console.log("Document found");
